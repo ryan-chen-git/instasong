550a84c7437a1f2f51d09c80b6bd631b8efcf05c
Ryan Chen <ryan_chen@berkeley.edu>
2025-06-11 16:17:58 -0700

    reformatted code

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const vision = require('@google-cloud/vision')
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+const vision = require("@google-cloud/vision");
 
-async function analyzeImage(image){
-    try{
-        const client = new vision.ImageAnnotatorClient({
-            keyFilename: 'google-cloud-vision-key.json'
-        });
+app.use(express.static(path.join(__dirname, "public")));
 
-        const [labels] = await client.labelDetection(image);
-        const [safeSearch] = await client.safeSearchDetection(image);
-        return {labels, safeSearch}
-    } catch(error){
-        console.error('Error:', error);
-    }
+const upload = multer({ dest: "uploads/" });
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file); // Logs file info to terminal
+  res.send("Image uploaded!");
+});
+
+async function analyzeImage(image) {
+  try {
+    const client = new vision.ImageAnnotatorClient({
+      keyFilename: "google-cloud-vision-key.json",
+    });
+
+    const [labels] = await client.labelDetection(image);
+    const [safeSearch] = await client.safeSearchDetection(image);
+    return { labels, safeSearch };
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
-(async()=>{
-    const imageAn = await analyzeImage('images/image.png');
-    console.log(imageAn.labels.labelAnnotations)
-} )()
+(async () => {
+  const imageAn = await analyzeImage("images/image.png");
+  console.log(imageAn.labels.labelAnnotations);
+})();
